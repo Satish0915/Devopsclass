8ec7ab348ee8f8d48635c3cab61171c0c79eb9ec
Satish0915 <satudhanani@gmail.com>
2023-04-19 14:17:17 -0400

    1.php modified for webhookjob

---
+++ 1.php
@@ -0,0 +1,9 @@
+<?php
+$orig_dec = 66251125;
+$dec2bin = decbin($orig_dec);
+$bin2dec = bindec($dec2bin);
+
+echo "original decimal number: $orig_dec <br>";
+echo "new binary number: $dec2bin <br>";
+echo "back to decimal: $bin2dec <br>";
+?>
